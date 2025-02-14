7cccfaccb2647545eb639c29e5c677872c336f3c
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-14 12:53:19 +0330

    js 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const wrapper=document.querySelector(".wrapper");
+const img=document.querySelector(".img img");
+const cancel=document.querySelector(".cancel");
+const fileName=document.querySelector(".fileName");
+const file=document.querySelector(".file");
+const btn=document.querySelector(".btn");
+
+btn.addEventListener("click",function(){
+    file.click();
+})
